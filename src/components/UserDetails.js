842a5dabfdf2478065cfff9b8714c918a5aa8f3a
Kevin Langat <kevin.langat@student.moringaschool.com>
2024-08-06 12:03:37 +0300

    Adds src/components/UserDetails.js

--- src/components/UserDetails.js
+++ src/components/UserDetails.js
@@ -1,19 +1,34 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import { useParams } from 'react-router-dom'
 
 function UserDetails() {
-    const [user, setUsers] =useState({})
+    const [user, setUser] =useState({})
     
-const params = useParams()
-console.log(params);
+const {id} = useParams()
+//console.log(params);
 
+useEffect(()=>{
+fetch(`http://localhost:3000/users/${id}`)
+.then(res => res.json())
+.then(user => {setUser(user)}
+)
+},[id])
+
+ if(!user.name){
+    return <div>Loading...</div>
+ }
 
 
   return (
     <div>
-      
-    </div>
-  )
+        <h1>UserDetails</h1>
+        <h3>Name: {user.name}</h3>
+        <h3>Username: {user.username}</h3>
+        <h3>Phone: {user.phone}</h3>
+        <h3>Email: {user.email}</h3>
+        <h3>Company: {user.company.name}</h3>
+ </div>
+        )
 }
 
 export default UserDetails
